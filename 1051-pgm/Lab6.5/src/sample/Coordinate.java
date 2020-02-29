38cc6f0c24d77db7a9a9cee529db37027714c06e
Ben Godwin <ben.godwin1@gmail.com>
2020-02-28 23:00:00 -0500

    add documentation for Coordinate class

--- 1051-pgm/Lab6.5/src/sample/Coordinate.java
+++ 1051-pgm/Lab6.5/src/sample/Coordinate.java
@@ -1,22 +1,29 @@
 package sample;
 
+// Class that holds an x and y Coordinate to make it easier to reuse
+// points for drawing javafx Shapes
+
 public class Coordinate {
     private final double x;
     private final double y;
 
     public Coordinate(double x, double y) {
-        this.x = (int) (((double) x / UseGUI3.MAX_X) * UseGUI3.MAX_X);
-        this.y = (int) (((double) y / UseGUI3.MAX_Y) * UseGUI3.MAX_Y);
+        this.x = x;
+        this.y = y;
     }
 
+    // formats the Coordinate into a human readable String in the form of
+    // "(x, y)" for printing or debugging
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
 
+    // getter for x coordinate
     public double x() {
         return x;
     }
 
+    // getter for y coordinate
     public double y() {
         return y;
     }
